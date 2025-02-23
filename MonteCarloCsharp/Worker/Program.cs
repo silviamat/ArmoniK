a70a5db82fe3f1c6dd00820c54f8b44a01955cb9
silviamat <103664459+silviamat@users.noreply.github.com>
2025-02-23 21:11:26 +0100

    csharp

--- MonteCarloCsharp/Worker/Program.cs
+++
@@ -1,6 +0,0 @@
-using ArmoniK.Api.Worker.Utils;
-using MonteCarlo.Worker;
-
-// Create the application and register the hello world example worker implementation
-WorkerServer.Create<BasketSimulationWorker>()
-            .Run();
